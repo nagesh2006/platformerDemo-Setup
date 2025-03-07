390e7740da7de17a9aaf347f1a21139effa2cb99
nagesh2006 <123nagesh2059@sjcem.edu.in>
2025-03-07 21:49:51 +0530

    coins

---
+++ script/coin_handle.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+@onready var anim = $AnimatedSprite2D
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.name == "player":
+		var tween = create_tween()
+		tween.tween_property(self, "position", Vector2(0, -40), 0.5)
+		tween.tween_property(self, "modulate:a", 0.0, 0.5)
+		GameManager.coins += 10
+		
+		tween.tween_callback(self.queue_free)
--- script/game_manager.gd
+++ script/game_manager.gd
@@ -2,3 +2,9 @@ extends Node
 
 var coins = 0
 var score = 0
+
+@onready var CoinValue = $GUI/coinValue
+
+func _process(delta: float) -> void:
+	CoinValue.text = str(coins)
+	
